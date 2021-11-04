11f2d5d90d314a40e13b97bc2cf35b2b3742cd0a
syhien <syhien@outlook.at>
2021-11-04 21:28:14 +0800

    增加需要的controller，增加对cell类

---
+++ ITSC/TemplateTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TemplateTableViewCell.swift
+//  ITSC
+//
+//  Created by nju on 2021/11/4.
+//
+
+import UIKit
+
+class TemplateTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var detail: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
