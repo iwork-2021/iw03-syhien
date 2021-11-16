9e7330479a52070a9ed5228cfade471bbaf0fd0d
syhien <syhien@outlook.at>
2021-11-16 13:29:15 +0800

    支持图文显示

--- ITSC/ArticleViewController.swift
+++ ITSC/ArticleViewController.swift
@@ -22,15 +22,36 @@ class ArticleViewController: UIViewController {
            let html = try? String(contentsOf: url),
            let doc = try? SwiftSoup.parse(html) {
             print("来了")
-            textTextView.text! = ""
+            textTextView.attributedText = NSAttributedString(string: "")
             if let content = try? doc.select(".wp_articlecontent").first() {
                 for i in content.children() {
+                    if let img = try? i.getElementsByTag("img") {
+                        if let imgURL = try? img.attr("src") {
+                            print("来咯来咯")
+                            if imgURL != "" {
+                                print(imgURL)
+                                let imgData = try! Data(contentsOf: URL(string: "https://itsc.nju.edu.cn" + imgURL)!)
+                                let image = UIImage(data: imgData)
+                                let attach = NSTextAttachment(image: image!)
+                                let imageWidth = textTextView.frame.width - 20
+                                let imageHeight = image!.size.height / image!.size.width * imageWidth
+                                attach.bounds = CGRect(x: 0, y: 0, width: imageWidth, height: imageHeight)
+                                let imgString = NSAttributedString(attachment: attach)
+                                var mutableString = NSMutableAttributedString(attributedString: textTextView.attributedText!)
+                                mutableString.append(imgString)
+                                textTextView.attributedText = mutableString
+                            }
+                        }
+                        
+                    }
                     if try! i.text().isEmpty {
                         continue
                     }
-//                    textTextView.text! += "        "
-                    textTextView.text! += try! i.text()
-                    textTextView.text! += "\n"
+                    var mutableString = NSMutableAttributedString(attributedString: textTextView.attributedText!)
+                    mutableString.append(NSAttributedString(string: try! i.text() + "\n"))
+                    textTextView.attributedText = mutableString
+//                    textTextView.text! += try! i.text()
+//                    textTextView.text! += "\n"
                 }
             }
         }
